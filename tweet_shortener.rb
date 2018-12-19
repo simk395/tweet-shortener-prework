f2a6c96f9ae1f9f670a07a92715387d6dc3b9ae1
Simon Kong <simon.kong95@gmail.com>
2018-12-19 18:59:16 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,15 @@
 # Write your code here.
+require "pry"
+def dictionary
+  dic = {
+    "hello" => "hi",
+    ["to", "two", "too"] => "2",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&",
+  }
+  binding.pry
+end
+
+dictionary
